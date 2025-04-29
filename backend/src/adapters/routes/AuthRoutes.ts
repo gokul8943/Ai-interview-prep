210303bfd9f726ba53a4bf1d2250af40d1262678
Gokul M R <gokulmr331@gmail.com>
2025-04-29 16:01:11 +0530

    feat;Interview page ui created

--- backend/src/adapters/routes/AuthRoutes.ts
+++ backend/src/adapters/routes/AuthRoutes.ts
@@ -33,7 +33,7 @@ router.post('/sign-in', async(req, res) =>{
         res.status(500).json({ message: "Error in routes", error  })
     }
 });
-router.post('verify-otp', async (req, res) => {
+router.post('/verify-otp', async (req, res) => {
     try {
         authController.verifyOtp(req, res)
     } catch (error) {
