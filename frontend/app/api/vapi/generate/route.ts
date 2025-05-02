2808e05b71ab523154551ed89c27042ee6d89ead
Gokul M R <gokulmr331@gmail.com>
2025-05-02 10:42:24 +0530

    feat:Google api and prompt created

---
+++ frontend/app/api/vapi/generate/route.ts
@@ -0,0 +1,30 @@
+import { generateText } from "ai";
+import { google } from "@ai-sdk/google";
+
+export async function GET(){
+    return Response.json({success:true,data:"Thank you "},{status:200});
+}
+
+export async function POST(request:Request){
+    const {type,role,level,techstack,amount,userId} = await request.json();
+    try {
+        const {text:questions} = await generateText({
+            model:google('gemini-2.0-flash-001'),
+            prompt:`Prepare questions for a job interview.
+            The job role is ${role}.
+            The job experience level is ${level}.
+            The tech stack is used in the job is ${techstack}.
+            The focus between behavioural  and technical questions should lean towards : ${type}.
+            The an\mount of questions required is ${amount}.
+            Please return the only the questions , wihout any additional text.
+                    The questions are going to be read by a voice assistant so do not use "/" or "*" or any other special characters which might break the voice assistant.
+        Return the questions formatted like this:
+        ["Question 1", "Question 2", "Question 3"]
+        
+        Thank you! <3`
+        })
+    } catch (error) {
+        console.log(error);
+        return Response.json({success:false,data:"Something went wrong"},{status:500});        
+    }
+}
