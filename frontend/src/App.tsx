0fa25943c18d712b22b1b64e4eae8c104e22088f
Gokul M R <gokulmr331@gmail.com>
2025-05-09 21:31:43 +0530

    update:neccessary file added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,10 +1,11 @@
+import AuthForm from "@/components/AuthForm";
 
 const SignUp = () => {
   return (
     <div>
-      
+      <AuthForm type="sign-up" />
     </div>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
